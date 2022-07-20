b4e369fa1703a6fbade6e50de2c2acef63dc27f0
IonVasilasco <ion.vasilasco@amdaris.com>
2022-07-20 14:21:32 +0300

    Changed Hello to Amdaris

--- VersionControl/VersionControl/Program.cs
+++ VersionControl/VersionControl/Program.cs
@@ -6,7 +6,7 @@ namespace VersionControl
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Amdaris!");
         }
     }
 }
